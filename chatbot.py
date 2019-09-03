e8d0ce63e5ba05a23efeeb91fb07b72333e8c035
Mammadli <il51ycyn@fauad.fau.de>
2019-09-03 12:09:54 +0200

    testing commit

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,10 @@ def chunks(l, n):
 
 #Helper function to get messages between Bots and users
 def getMessages():
-    response = requests.get('https://api.cai.tools.sap/connect/v1/conversations/' + data['nlp']['uuid'],
+    # response = requests.get('https://api.cai.tools.sap/connect/v1/conversations/' + data['nlp']['uuid'],
+    #   headers={'Authorization': '54187a3945f3af9ea86d40ebca0400f2'}
+    # )
+    response = requests.get('https://api.cai.tools.sap/connect/v1/conversations/' + 'f727adaa-90fd-455d-b855-c2d656df66f4',
       headers={'Authorization': '54187a3945f3af9ea86d40ebca0400f2'}
     )
     conv_dictionary = json.loads(response.text)
