f32c33602ed3234b3fafde5dfe9d2232153b4f76
Mammadli <il51ycyn@fauad.fau.de>
2019-09-20 13:30:52 +0200

    messages_info Table insertation

--- chatbot.py
+++ chatbot.py
@@ -4,9 +4,9 @@ import requests
 import pprint as pp
 import sapcai
 import os 
-# import psycopg2
 import logging
 from db_connect import DBConnect
+import re
 
 app = Flask(__name__)
 port = int(os.environ["PORT"])
@@ -35,71 +35,40 @@ def getMessages():
     else:
         logging.log(logging.WARNING, 'Empty Response Object')
 
-# def update_bad_conv():
-#     conn = psycopg2.connect(dbname="d51dpnoammut78", user="mfeteccnqkvtor", 
-#             password="8c505e55eb950c9b8a5a8a3fb3118b103fc7dabac9b7eb0737b156c9f695fad5", 
-#             host='ec2-54-217-235-87.eu-west-1.compute.amazonaws.com')
-#     cur = conn.cursor()
-#     cur.execute("SELECT bad_conv FROM messages LIMIT 1;")
-#     counter = cur.fetchone()[0]
-#     counter = counter + 1
-#     cur.execute("UPDATE messages SET bad_conv = %s", [counter])
-#     conn.commit()
-#     cur.close()
-#     conn.close()
+def insert_message_info():
+    response = requests.get('https://api.cai.tools.sap/connect/v1/conversations/' + data['conversation']['id'],
+      headers={'Authorization': '54187a3945f3af9ea86d40ebca0400f2'}
+    )
+    response.json()
+    l = ''
+    for i in range(len(d['results']['messages'])):
+        l = l + ' ' + str(d['results']['messages'][i]['attachment']['content'])
 
-# def update_good_conv():
-#     conn = psycopg2.connect(dbname="d51dpnoammut78", user="mfeteccnqkvtor", 
-#             password="8c505e55eb950c9b8a5a8a3fb3118b103fc7dabac9b7eb0737b156c9f695fad5", 
-#             host='ec2-54-217-235-87.eu-west-1.compute.amazonaws.com')
-#     cur = conn.cursor()
-#     cur.execute("SELECT good_conv FROM messages LIMIT 1;")
-#     counter = cur.fetchone()[0]
-#     counter = counter + 1
-#     cur.execute("UPDATE messages SET good_conv = %s", [counter])
-#     conn.commit()
-#     cur.close()
-#     conn.close()
+    len_of_message = len(re.split('Thank you!!! | Took a note on that ', l)[-1].split(' '))
+    my_con = DBConnect("d51dpnoammut78","mfeteccnqkvtor","8c505e55eb950c9b8a5a8a3fb3118b103fc7dabac9b7eb0737b156c9f695fad5",
+                    'ec2-54-217-235-87.eu-west-1.compute.amazonaws.com')
 
-
-
-# def db_connect_insert():
-#     conn = psycopg2.connect(dbname="d51dpnoammut78", user="mfeteccnqkvtor", 
-#             password="8c505e55eb950c9b8a5a8a3fb3118b103fc7dabac9b7eb0737b156c9f695fad5", 
-#             host='ec2-54-217-235-87.eu-west-1.compute.amazonaws.com')
-#     cur = conn.cursor()
-    
-    # i = getMessages()
-    # while True:
-    #     try:
-    #         cur.execute("INSERT INTO messages (user_messages, bot_messages) VALUES (%s, %s)", tuple(next(i)))
-    #     except StopIteration:
-    #         break
-            
-    # conn.commit()
-    # cur.close()
-    # conn.close()
+    if state == 'no':
+        my_con.db_connection()
+        my_con.cur.execute("INSERT INTO messages_info(bad_mess_len) VALUES(%s);", len_of_message)
+        my_con.end_connection()    
 
 @app.route('/', methods=['POST'])
 def index():
     global data
-    # if request.get_data():
-    
-    # return render_template('home.html', data=data)     
-    # try:
-    #     data = json.loads(request.get_data())
-    # except ValueError:  # includes simplejson.decoder.JSONDecodeError
-    #     print('Decoding JSON has failed')
+
     my_con = DBConnect("d51dpnoammut78","mfeteccnqkvtor","8c505e55eb950c9b8a5a8a3fb3118b103fc7dabac9b7eb0737b156c9f695fad5",
                     'ec2-54-217-235-87.eu-west-1.compute.amazonaws.com')
+    
     data = json.loads(request.get_data())
     if data['nlp']['intents'][0]['slug'] == 'no':
         my_con.update_bad_conv()
+        insert_message_info()
         return jsonify( 
         status=200, 
         replies=[{ 
           'type': 'text', 
-          'content': 'Took a note on that', 
+          'content': 'Take a note on that!',
         }], 
         conversation={ 
           'memory': { 'key': 'value' } 
--- db_connect.py
+++ db_connect.py
@@ -11,28 +11,32 @@ class DBConnect():
 
     def db_connection(self):
         self.conn = psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host)
+        self.cur = self.conn.cursor()
 
     def end_connection(self):
+        self.conn.commit()
+        self.cur.close()
         self.conn.close()
 
     def update_bad_conv(self):
         self.db_connection()
-        self.cur = self.conn.cursor()
         self.cur.execute("SELECT bad_conv FROM messages LIMIT 1;")
         counter = self.cur.fetchone()[0]
         counter = counter + 1
         self.cur.execute("UPDATE messages SET bad_conv = %s", [counter])
-        self.conn.commit()
-        self.cur.close()
         self.end_connection()
 
     def update_good_conv(self):
         self.db_connection()
-        self.cur = self.conn.cursor()
         self.cur.execute("SELECT good_conv FROM messages LIMIT 1;")
         counter = self.cur.fetchone()[0]
         counter = counter + 1
         self.cur.execute("UPDATE messages SET good_conv = %s", [counter])
-        self.conn.commit()
-        self.cur.close()
         self.end_connection()
+
+    def db_insert(self, id, message_len, avr_message):
+        self.db_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute("INSERT INTO message_info(id, message_len, avr_message) VALUES(%s, %s, %s);", [id, message_len,avr_message])
+        self.end_connection()
+
