77cfe9505cdb84c1d7cd2acdf1d4c7184fed26dc
Mammadli <il51ycyn@fauad.fau.de>
2019-09-20 15:50:27 +0200

    messages_info Table insertation

--- chatbot.py
+++ chatbot.py
@@ -53,7 +53,6 @@ def insert_message_info(state):
         my_con.cur.execute("INSERT INTO messages_info(bad_mess_len) VALUES(%s);", [len_of_message])
         my_con.cur.execute("SELECT ROUND(AVG(bad_mess_len)) FROM messages_info;")
         av = my_con.cur.fetchone()[0]
-        av = av + 1 
         my_con.cur.execute("UPDATE messages_info SET bad_mess_avr = %s",[av])
         my_con.end_connection()
     elif state == 'yes':
@@ -61,7 +60,6 @@ def insert_message_info(state):
         my_con.cur.execute("INSERT INTO messages_info(good_mess_len) VALUES(%s);", [len_of_message])
         my_con.cur.execute("SELECT ROUND(AVG(good_mess_len)) FROM messages_info;")
         av = my_con.cur.fetchone()[0]
-        av = av + 1
         my_con.cur.execute("UPDATE messages_info SET good_mess_avr = %s",[av])
         my_con.end_connection()
             
